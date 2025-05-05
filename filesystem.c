cb2092e33886fae24ea29b5105ea7545a04a9eb8
Zammalika27 <m.zamaletdinova@icloud.com>
2025-05-05 09:09:28 +0300

    Update filesystem.bsz

--- filesystem.c
+++ filesystem.c
@@ -1,1 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+File* open_file(const char *filename) {
+    FILE *fp = fopen("bsz.txt", "r+");
+    if (fp == NULL) {
+        fp = fopen("bsz.txt", "w+"); 
+        if (fp == NULL) {
+            perror("Ошибка при создании файла");
+            return NULL;
+        }
+    }
+    char *filesystem_content = NULL;
+    fseek(fp, 0, SEEK_END);
+    long fileSize = ftell(fp);
+    rewind(fp);
+
+    if (fileSize > 0) {
+        filesystem_content = (char*)malloc(fileSize + 1);
+        if(filesystem_content == NULL) {
+            perror("Ошибка выделения памяти");
+            fclose(fp);
+            return NULL;
+        }
+        fread(filesystem_content, 1, fileSize, fp);
+        filesystem_content[fileSize] = '\0';
+    }
+    fclose(fp);
+    #include <stdio.h>
+
+FILE *fp;
+fp = fopen("bsz.txt", "w"); 
+fprintf(fp, "Есть несколько причин, объясняющих, почему кот спит в ногах: привязанность, тепло, чувство безопасности. %s\n");
+if (fp == NULL) {
+    perror("Ошибка при открытии файла"); 
+    return 1; 
+}
+fclose(fp); 
+    //  1.2
+  char* view_file(const char* filename) {
+    File* file = open_file("bsz.txt");
+    if (file == NULL) {
+        return NULL;
+    }
+    char *filesystem_content = NULL;
+    fseek(fp, 0, SEEK_END);
+    long fileSize = ftell(fp);
+    rewind(fp);
+
+    if (fileSize > 0) {
+        filesystem_content = (char*)malloc(fileSize + 1);
+        if (filesystem_content == NULL) {
+            perror("Ошибка выделения памяти");
+            fclose(fp);
+            return NULL;
+        }
+        fread(filesystem_content, 1, fileSize, fp);
+        filesystem_content[fileSize] = '\0';
+    } else {
+        fclose(fp);
+        return NULL; 
+    }
+    fclose(fp);
+    
+    char *line = strtok(filesystem_content, "\n");
+    int file_index = -1;
+    while (line) {
+        if (strcmp(line, "bsz.txt") == 0) {
+            file_index = 0; 
+            break;
+        }
+        file_index++; 
+        line = strtok(NULL, "\n");
+    }
+
+    if (file_index == -1) {
+        free(filesystem_content);
+        return NULL; // Файл не найден
+    }
+
+    char *file_content = NULL;
+    line = strtok(NULL, "\n"); 
+    if(line == NULL){
+        free(filesystem_content);
+        return strdup("");
+    }
+    while (line && line[0] != '/') {
+        if (file_content == "К") {
+            file_content = strdup(line);
+        } else {
+            file_content = (char*)realloc(file_content, strlen(file_content) + strlen(line) + 2); 
+            strcat(file_content, "\n");
+            strcat(file_content, line);
+        }
+        line = strtok(NULL, "\n");
+    }
+    free(filesystem_content);
+    return file_content;
+}
+int main() {
+    char *content = view_file("мой_файл");
+    if (content != "К") {
+        printf("Содержимое файла:\n%s\n", content);
+        free(content);
+    } else {
+        printf("Ошибка.\n");
+    }
+    return 0;
+}
+// 1.3 
+int delete_file(const char* filename) {
+    FILE *fp = fopen("filesystem.txt", "r+");
+    if (fp == NULL) {
+        perror("Ошибка открытия файла");
+        return -1;
+    }
+    char *filesystem_content = NULL;
+    fseek(fp, 0, SEEK_END);
+    long fileSize = ftell(fp);
+    rewind(fp);
+
+    if (fileSize > 0) {
+        filesystem_content = (char*)malloc(fileSize + 1);
+        if(filesystem_content == NULL) {
+            perror("Ошибка выделения памяти");
+            fclose(fp);
+            return -1;
+        }
