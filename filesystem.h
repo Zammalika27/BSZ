d938956a60ce7d5db93653115189bd619f83c668
Zammalika27 <m.zamaletdinova@icloud.com>
2025-05-06 13:40:43 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -1,31 +1,22 @@
 #ifndef FILESYSTEM_H
 #define FILESYSTEM_H
 
-typedef struct {
+typedef struct
+{
     char *path;
     char *content;
-} FileEntry;
+    } 
+FileEntry;
 
-typedef struct {
+typedef struct 
+{
     FileEntry *entries;
     size_t     count;
 } FileSystem;
 
-/* загрузка / сохранение / освобождение */
-int  fs_load (const char *fname, FileSystem *fs);
-int  fs_save (const char *fname, const FileSystem *fs);
-void fs_free (FileSystem *fs);
-
-/* CRUD */
-int  fs_select(const FileSystem *fs, const char *path, char **out_content);
-int  fs_insert(FileSystem *fs, const char *path);
-int  fs_update(FileSystem *fs, const char *path, const char *new_content);
-int  fs_delete(FileSystem *fs, const char *path);
-
-/* новые функции */
-int    fs_crypto (FileSystem *fs, const char *path, const char *key);
-int    fs_decrypto(FileSystem *fs, const char *path, const char *key);
-size_t fs_count  (const FileSystem *fs);
-int    fs_rename (FileSystem *fs, const char *old_path, const char *new_path);
-
-#endif /* FILESYSTEM_H */
+FILE* open_file(const char *fname);
+char *view_file(const char *fname);
+int delete_file(const char *fname);
+char *new_filesystem ();
+int modify_file(const char *fname, const char *new_content);
+#endif 
