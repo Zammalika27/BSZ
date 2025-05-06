e9dcf23340867220bab9e4e2d2d0f5302f9889e4
Zammalika27 <m.zamaletdinova@icloud.com>
2025-05-06 14:08:23 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,43 @@
 #include "filesystem.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main () 
+{
+FILE* open_file(const char *bsz.txt) {
+    FILE *fp = fopen("bsz.txt", "r+");
+    if (fp == NULL) {
+        fp = fopen("bsz.txt", "w+"); 
+        if (fp == NULL) {
+            perror("Ошибка при создании файла");
+            return NULL;
+        }
+    }
+char *filesystem_content = NULL;
+    fseek(fp, 0, SEEK_END);
+    long fileSize = ftell(fp);
+    rewind(fp);
+
+    if (fileSize > 0) {
+        filesystem_content = (char*)malloc(fileSize + 1);
+        if(filesystem_content == NULL) {
+            perror("Ошибка выделения памяти");
+            fclose(fp);
+            return NULL;
+        }
+        fread(filesystem_content, 1, fileSize, fp);
+        filesystem_content[fileSize] = '\0';
+    }
+    fclose(fp);
+    return 1;
+}
+fprintf(*bsz.txt, "%s.\n");
+  int readFileAndPrint(const char *bsz.txt) {
+  if (fp == NULL) {
+    perror("Ошибка при открытии файла для чтения");
+    return 1; }
+  fgetc ("bsz.txt"); 
+  }
+  fclose ("bsz.txt");
+  return 0;
+}
